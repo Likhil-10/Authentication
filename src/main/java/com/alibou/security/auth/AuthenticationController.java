b2d94d4dcb19514bc3d6fc7619b17b09e31d8aa9
Likhil <likhil1234@gmail.com>
2024-04-15 12:57:07 +0530

    commited

--- src/main/java/com/alibou/security/auth/AuthenticationController.java
+++ src/main/java/com/alibou/security/auth/AuthenticationController.java
@@ -1,15 +1,15 @@
 package com.alibou.security.auth;
 
+import jdk.jfr.ContentType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin("*")
+
 public class AuthenticationController {
 
     private final AuthenticationService service;
